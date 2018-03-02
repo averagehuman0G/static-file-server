27cb0b7fd9aaa78967666b8189e55f92074c7c23
Jonathan Vargas <jonathanv3232@gmail.com>
2018-03-01 22:51:02 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const http = require('http');
+
+//Create server
+
+//get the path to the file
+
+//check if requested file exist and if it is indeed a file
+
+//create a read stream if all is good and read the file
+// pipe it to the response strem
+
