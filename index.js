17b8b1188ee3f18085624e0f291a3aa4c0e0234e
Jonathan Vargas <jonathanv3232@gmail.com>
2018-04-04 23:42:29 -0400

    adds a server

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const fs = require('fs');
 const http = require('http');
 
 //Create server
+const http = require('http');
+http
+  .createServer((req, res) => {
+    // server code
+    console.log(`${req.method} ${req.url}`);
+    res.end('Just a server');
+  })
+  .listen(4000);
 
 //get the path to the file
 
@@ -9,4 +17,3 @@ const http = require('http');
 
 //create a read stream if all is good and read the file
 // pipe it to the response strem
-
